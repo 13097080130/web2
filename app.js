291140cf8773fdf70016831a02aa5cd441b9e49b
13097080130 <2268942115@qq.com>
2021-05-13 11:08:46 +0800

    '添加数据库'

--- app.js
+++ app.js
@@ -11,7 +11,27 @@ var registerRouter = require('./routes/register');
 
 var app = express();
 let router = express.Router();
-let db = require('./model/db'); 
+let db = require('./model/db');
+
+Date.prototype.format = function(format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(), //day
+        "h+": this.getHours(), //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(format))
+            format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +43,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
-  extended:true
+    extended: true
 }))
 
 // app.all('*', function(req, res, next) {
@@ -36,48 +56,48 @@ app.use(bodyParser.urlencoded({
 // });
 
 //设置跨域访问
-app.use(function (req, res, next) {
-  if (req.method === "OPTIONS") {
-    let headers = {};
-    headers["Access-Control-Allow-Origin"] = "*";
+app.use(function(req, res, next) {
+    if (req.method === "OPTIONS") {
+        let headers = {};
+        headers["Access-Control-Allow-Origin"] = "*";
 
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
 
-    headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Allow-Credentials"] = false;
 
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
 
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
 
-    res.writeHead(200, headers);
+        res.writeHead(200, headers);
 
-    res.end();
-  } else {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  }
+        res.end();
+    } else {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        next();
+    }
 });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/register',registerRouter)
+app.use('/register', registerRouter)
 
 //插入测试
-app.get('/dbtest',function(req, res){
-    let project = {project_name: 'test', create_time: '2017-03-28 14:09:29'};
+app.get('/dbtest', function(req, res) {
+    let project = { project_name: 'test', create_time: '2017-03-28 14:09:29' };
     let sqlString = 'INSERT INTO project SET ?';
     let connection = db.connection();
-    db.insert(connection, sqlString, project, function(id){
+    db.insert(connection, sqlString, project, function(id) {
         console.log('inserted id is:' + id);
     });
     db.close(connection);
-	res.send(JSON.stringify(project))
+    res.send(JSON.stringify(project))
     return;
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 
@@ -97,13 +117,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/register.js
+++ routes/register.js
@@ -1,22 +1,23 @@
 var express = require('express');
+let db = require('./model/db');
 var router = express.Router();
 
 /* GET register listing. */
 router.get('/', function(req, res, next) {
-  let inform = req.query;
-  let username = inform.newusername;
-  let password = inform.newpassword;
-  let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是："+ password;
-  res.send(result)
+    let inform = req.query;
+    let username = inform.newusername;
+    let password = inform.newpassword;
+    let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是：" + password;
+    res.send(result)
 });
 
 router.post('/', function(req, res, next) {
-  let inform = req.body;
-  // console.log(inform)
-  let username = inform.params.newusername;
-  let password = inform.params.newpassword;
-  let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是："+ password;
-  res.send(result)
-  });
+    let inform = req.body;
+    // console.log(inform)
+    let username = inform.params.newusername;
+    let password = inform.params.newpassword;
+    let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是：" + password;
+    res.send(result)
+});
 
 module.exports = router;
