66735a304e4c54003e1ad01821c1c91f5b460dc4
13097080130 <2268942115@qq.com>
2021-05-18 08:44:42 +0800

    '111'

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,48 +12,20 @@ var app = express();
 let router = express.Router();
 let db = require('./model/db');
 
-Date.prototype.format = function(format) {
-    var o = {
-        "M+": this.getMonth() + 1, //month
-        "d+": this.getDate(), //day
-        "h+": this.getHours(), //hour
-        "m+": this.getMinutes(), //minute
-        "s+": this.getSeconds(), //second
-        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
-        "S": this.getMilliseconds() //millisecond
-    }
-    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
-        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(format))
-            format = format.replace(RegExp.$1,
-                RegExp.$1.length == 1 ? o[k] :
-                ("00" + o[k]).substr(("" + o[k]).length));
-    return format;
-}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-
 // app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   next();
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By", ' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
 // });
-
 //设置跨域访问
 app.use(function(req, res, next) {
     if (req.method === "OPTIONS") {
@@ -78,9 +49,17 @@ app.use(function(req, res, next) {
     }
 });
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter)
+
+
 
 //插入测试
 app.get('/dbtest', function(req, res) {
--- routes/register.js
+++ routes/register.js
@@ -1,23 +1,31 @@
 var express = require('express');
-let db = require('./model/db');
 var router = express.Router();
 
-/* GET register listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
     let inform = req.query;
-    let username = inform.newusername;
-    let password = inform.newpassword;
-    let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是：" + password;
-    res.send(result)
+
+    let username = inform.newUsername;
+    let password = inform.newPassword;
+    let result = {
+        state: 'success',
+        message: "注册成功!!!您的账号为" + username
+    };
+    res.send(result);
 });
 
 router.post('/', function(req, res, next) {
-    let inform = req.body;
-    // console.log(inform)
-    let username = inform.params.newusername;
-    let password = inform.params.newpassword;
-    let result = "恭喜你注册成功了，你的用户名:" + username + "\n" + "你的密码是：" + password;
-    res.send(result)
+    let inform = req.body.params;
+    console.log(req);
+
+    let username = inform.newUsername;
+    let password = inform.newPassword;
+    let result = {
+        state: 'success',
+        message: "注册成功!!!您的账号为" + username
+    };
+    res.send(result);
+
 });
 
 module.exports = router;
